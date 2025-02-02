4fe4e426cb0e99ce46c8f750ddfb72dba4aff500
Tahera Akter Mukta <Taheraaktermukta17@gmail.com>
2025-02-02 18:50:24 +0000

    Initial Commit

---
+++ activeMeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  activeMeApp
+//
+//  Created by Tahera Akter Mukta on 02/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ activeMeApp/activeMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  activeMeAppApp.swift
+//  activeMeApp
+//
+//  Created by Tahera Akter Mukta on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct activeMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
