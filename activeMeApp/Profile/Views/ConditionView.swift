3d96c7a4260671a37072aa42c772f8c422d6c20e
Tahera Akter Mukta <Taheraaktermukta17@gmail.com>
2025-02-12 22:12:33 +0000

    Section 5.2: I just created a few UI views for the profile page.

---
+++ activeMeApp/Profile/Views/ConditionView.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionView.swift
+//  activeMeApp
+//
+//  Created by Tahera Akter Mukta on 12/02/2025.
+//
+
+import SwiftUI
+
+struct ConditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionView()
+}
---
+++ activeMeApp/Profile/Views/PrivacyView.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacyView.swift
+//  activeMeApp
+//
+//  Created by Tahera Akter Mukta on 12/02/2025.
+//
+
+import SwiftUI
+
+struct PrivacyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrivacyView()
+}
